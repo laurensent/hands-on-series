bc14d634b7a34c0949afdf9b6c46257df8da611f
Lauren <63347384+laurensent@users.noreply.github.com>
2025-12-25 10:14:37 +0800

    Initial commit

---
+++ hands-on-spring/src/main/java/org/laurensent/spring/Application.java
@@ -0,0 +1,9 @@
+package org.laurensent.spring;
+
+public class Application {
+
+    public static void main(String[] args) {
+        System.out.println("Hands On Spring - Application Started");
+    }
+
+}
